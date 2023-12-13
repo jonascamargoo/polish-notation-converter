911f27a817e0b92149ecba9e85a287ce89c0634e
jonascamargoo <jonascamargo999@gmail.com>
2023-12-13 18:50:43 -0300

    feat: validateExpression is finished

--- tests/utils/ValidateExpression.spec.ts
+++ tests/utils/ValidateExpression.spec.ts
@@ -33,14 +33,13 @@ describe("ValidateExpression", () => {
     expect(validator.validateParenthesesExpression("(3+2")).toBe(false);
   });
 
-  // test("checkSyntaxExpression", () => {
-  //   expect(validator.checkSyntaxExpression("2+3")).toBe(true);
-  //   expect(validator.checkSyntaxExpression("")).toBe(false);
-  //   expect(validator.checkSyntaxExpression("(3+2)")).toBe(true);
-
-
+  test("checkSyntaxExpression", () => {
+    expect(validator.checkSyntaxExpression("2+3")).toBe(true);
+    expect(validator.checkSyntaxExpression("")).toBe(false);
+    expect(validator.checkSyntaxExpression("(3+2)")).toBe(true);
+    expect(validator.validateParenthesesExpression("((3+2)*5)")).toBe(true);
     
-  // });
+  });
 
   
 });
