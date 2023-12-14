71080a2be9477e387c9adee81e9e571b7fe714a5
jonascamargoo <jonascamargo999@gmail.com>
2023-12-14 19:17:05 -0300

    fix: fix parser file comments

---
+++ app/src/utils/ExpressionSolver.ts
@@ -0,0 +1,8 @@
+// import { parse } from './ExpressionParser';
+
+// export const solve = (postfixExpression: string): number => {
+    
+// }
+
+// // 2 3 4 * +
+// // 5 2 3 / -
--- app/tests/utils/ExpressionParser.spec.ts
+++ app/tests/utils/ExpressionParser.spec.ts
@@ -1,12 +1,12 @@
-import { parser } from '../../src/utils/ExpressionParser';
+import { parse } from '../../src/utils/ExpressionParser';
 
 describe('parser function', () => {
     it('simple expression', () => {
-      expect(parser('2 + 3 * 4')).toBe('2 3 4 * +');
+      expect(parse('2 + 3 * 4')).toBe('2 3 4 * +');
     });
   
     it('expression with parentheses', () => {
-      expect(parser('(5 + 3) * 2')).toBe('5 3 + 2 *');
+      expect(parse('(5 + 3) * 2')).toBe('5 3 + 2 *');
     });
   
 });
