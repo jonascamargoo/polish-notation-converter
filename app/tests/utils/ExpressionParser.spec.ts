577583ed09da69da6e3a8b172c8411abc308349c
jonascamargoo <jonascamargo999@gmail.com>
2023-12-17 12:25:38 -0300

    refactor: expression parser

--- app/tests/utils/ExpressionParser.spec.ts
+++ app/tests/utils/ExpressionParser.spec.ts
@@ -1,12 +1,37 @@
-import { parse } from '../../src/utils/ExpressionParser';
+import { ExpressionParser } from '../../src/utils/ExpressionParser';
 
-describe('parser function', () => {
-    it('simple expression', () => {
-      expect(parse('2 + 3 * 4')).toBe('2 3 4 * +');
-    });
-  
-    it('expression with parentheses', () => {
-      expect(parse('(5 + 3) * 2')).toBe('5 3 + 2 *');
-    });
-  
+describe('ExpressionParser class', () => {
+  it('should parse a simple expression with digits and parentheses', () => {
+    const expressionParser = new ExpressionParser();
+    const result = expressionParser.parse('(3 + 4) * 5');
+    expect(result).toEqual('3 4 + 5 *');
+  });
+
+  it('should handle an expression with operators and parentheses', () => {
+    const expressionParser = new ExpressionParser();
+    const result = expressionParser.parse('(2 + 3) * (4 - 1)');
+    expect(result).toEqual('2 3 + 4 1 - *');
+  });
+
+  it('should handle an expression with multiple operators and parentheses', () => {
+    const expressionParser = new ExpressionParser();
+    const result = expressionParser.parse('(1 + 2) * 3 ^ 2 - 4');
+    expect(result).toEqual('1 2 + 3 2 ^ * 4 -');
+  });
+
+  it('should handle an empty expression', () => {
+    const expressionParser = new ExpressionParser();
+    expect(() => expressionParser.parse('')).toThrow();
+  });
+
+  it('should handle an expression with digits and no parentheses or operators', () => {
+    const expressionParser = new ExpressionParser();
+    expect(() => expressionParser.parse('123456')).toThrow();
+  });
+
+  it('should handle an expression with only one type of token (operators)', () => {
+    const expressionParser = new ExpressionParser();
+    expect(() => expressionParser.parse('+-*/')).toThrow();
+  });
 });
+
