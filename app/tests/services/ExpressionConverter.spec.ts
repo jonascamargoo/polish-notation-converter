97c93a3b374a1b4d5da4dad6981045c285d01a79
jonascamargoo <jonascamargo999@gmail.com>
2023-12-21 10:42:56 -0300

    feat: add validatePostfix and its tests

--- app/tests/services/ExpressionConverter.spec.ts
+++ app/tests/services/ExpressionConverter.spec.ts
@@ -34,37 +34,37 @@ describe('ExpressionConverter class', () => {
     expect(() => expressionConverter.convertToPostfix('+-*/')).toThrow();
   });
 
-  // it('should convert a simple postfix expression with digits and parentheses to infix', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   const result = expressionConverter.convertToInfix('3 4 + 5 *');
-  //   expect(result).toEqual('(3 + 4) * 5');
-  // });
+  it('should convert a simple postfix expression with digits and parentheses to infix', () => {
+    const expressionConverter = new ExpressionConverter();
+    const result = expressionConverter.convertToInfix('3 4 + 5 *');
+    expect(result).toEqual('((3 + 4) * 5)');
+  });
 
-  // it('should convert an expression with operators and parentheses to infix', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   const result = expressionConverter.convertToInfix('2 3 + 4 1 - *');
-  //   expect(result).toEqual('(2 + 3) * (4 - 1)');
-  // });
+  it('should convert an expression with operators and parentheses to infix', () => {
+    const expressionConverter = new ExpressionConverter();
+    const result = expressionConverter.convertToInfix('2 3 + 4 1 - *');
+    expect(result).toEqual('((2 + 3) * (4 - 1))');
+  });
 
-  // it('should convert an expression with multiple operators and parentheses to infix', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   const result = expressionConverter.convertToInfix('1 2 + 3 2 ^ * 4 -');
-  //   expect(result).toEqual('(1 + 2) * 3 ^ 2 - 4');
-  // });
+  it('should convert an expression with multiple operators and parentheses to infix', () => {
+    const expressionConverter = new ExpressionConverter();
+    const result = expressionConverter.convertToInfix('1 2 + 3 2 ^ * 4 -');
+    expect(result).toEqual('(((1 + 2) * (3 ^ 2)) - 4)');
+  });
 
-  // it('should handle an empty expression', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   expect(() => expressionConverter.convertToInfix('')).toThrow();
-  // });
+  it('should handle an empty expression', () => {
+    const expressionConverter = new ExpressionConverter();
+    expect(() => expressionConverter.convertToInfix('')).toThrow();
+  });
 
-  // it('should handle an expression with digits and no parentheses or operators', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   expect(() => expressionConverter.convertToInfix('123456')).toThrow();
-  // });
+  it('should handle an expression with digits and no parentheses or operators', () => {
+    const expressionConverter = new ExpressionConverter();
+    expect(() => expressionConverter.convertToInfix('123456')).toThrow();
+  });
 
-  // it('should handle an expression with only one type of token (operators)', () => {
-  //   const expressionConverter = new ExpressionConverter();
-  //   expect(() => expressionConverter.convertToInfix('+-*/')).toThrow();
-  // });
+  it('should handle an expression with only one type of token (operators)', () => {
+    const expressionConverter = new ExpressionConverter();
+    expect(() => expressionConverter.convertToInfix('+-*/')).toThrow();
+  });
 });
 
